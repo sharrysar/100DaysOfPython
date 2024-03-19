d7db40dc18ac00c29f781326522a20287be27617
sharry <sharrysar@users.noreply.github.com>
2024-03-18 20:29:17 -0500

    day 16: OOP final

--- day16/main.py
+++ day16/main.py
@@ -2,4 +2,24 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
 
+is_on = True
+money = 0
+
+while is_on:
+    options = menu.get_items()
+    order = input(f"What would you like? ({options}): ")
+
+    if order == "off":
+        print("Turning off...")
+        is_on = False
+    elif order == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        item = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(item) and money_machine.make_payment(item.cost):
+            coffee_maker.make_coffee(item)
