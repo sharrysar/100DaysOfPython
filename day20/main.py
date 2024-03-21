79732c541faa9d483fecfd55a94bf48c0e7184f8
sharry <sharrysar@users.noreply.github.com>
2024-03-21 16:21:02 -0500

    day 20: Part 1 of Snake game; learning about animation and coordinates using Turtle

---
+++ day20/main.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(600,600)
+screen.bgcolor("black")
+screen.title("Snake")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+
+
+
+game_on = True
+
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+
+
+
+
+
+
+screen.exitonclick()
