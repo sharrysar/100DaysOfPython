dcbf68625fccbf1a01b9010fbd1b8ad097ce878b
sharry <sharrysar@users.noreply.github.com>
2024-03-21 22:27:19 -0500

    added a text file to save the high score so it's persistent

--- day20-day21/scoreboard.py
+++ day20-day21/scoreboard.py
@@ -7,17 +7,27 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0,280)
         self.hideturtle()
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write(arg=f"Game over.", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as file:
+                file.write(str(self.high_score))
+        
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def update_score(self):
         self.score += 1
-        self.clear()
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
