901181b18f661727446844c7c66ed0fef19c6f8e
sharry <sharrysar@users.noreply.github.com>
2024-04-05 01:01:46 -0500

    added saving data to txt file functionality; aligned elements

--- day29/main.py
+++ day29/main.py
@@ -4,7 +4,15 @@ def generate_password():
     pass
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    pass
+    website = website_input.get()
+    login = login_input.get()
+    password = password_input.get()
+
+    with open("data.txt", "a") as data:
+        data.write(f"{website} | {login} | {password}\n")
+        
+    website_input.delete(0, END)
+    password_input.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
 
 # window set up
@@ -21,25 +29,27 @@ canvas.grid(column=1, row=0)
 
 # labels
 website_label = Label(text="Website:")
-website_label.grid(column=0, row=1)
+website_label.grid(column=0, row=1, sticky="W")
 login_label = Label(text="Email/Username:")
-login_label.grid(column=0, row=2)
+login_label.grid(column=0, row=2, sticky="W")
 password_label = Label(text="Password: ")
-password_label.grid(column=0, row=3)
+password_label.grid(column=0, row=3, sticky="W")
 
 # inputs
 website_input = Entry(width=35)
-website_input.grid(column=1, row=1, columnspan=2)
+website_input.grid(column=1, row=1, columnspan=2, sticky="EW")
+website_input.focus()
 login_input = Entry(width=35)
-login_input.grid(column=1, row=2, columnspan=2)
-password_input = Entry(width=21)
-password_input.grid(column=1, row=3)
+login_input.grid(column=1, row=2, columnspan=2, sticky="EW")
+login_input.insert(0, "myemail@gmail.com")
+password_input = Entry(width=23)
+password_input.grid(column=1, row=3, sticky="EW")
 
 # buttons
 generate_button = Button(text="Generate Password", command=generate_password)
-generate_button.grid(column=2, row=3)
-add_button = Button(text="Add", width=36, command=save_password)
-add_button.grid(column=1, row=4, columnspan=2)
+generate_button.grid(column=2, row=3, sticky="EW")
+add_button = Button(text="Add", width=32, command=save_password)
+add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
 
 
 
